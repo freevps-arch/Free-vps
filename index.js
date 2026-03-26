7131d195a0c29e887dd7cc46f2bb8d200016698d
freevps-arch <freevpsgithub2222@gmail.com>
2026-03-26 21:12:03 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const mineflayer = require('mineflayer')
+
+const config = {
+  host: 'mineproness.aternos.me', // example: hypixel.net
+  port: 25565,
+  username: 'MinepronessAFK', // cracked = any name
+  password: undefined // only if premium account
+}
+
+let bot
+
+function createBot() {
+  bot = mineflayer.createBot(config)
+
+  bot.on('login', () => {
+    console.log('✅ Bot joined the server')
+  })
+
+  bot.on('spawn', () => {
+    console.log('🌍 Spawned in world')
+  })
+
+  bot.on('end', () => {
+    console.log('❌ Disconnected! Reconnecting...')
+    reconnect()
+  })
+
+  bot.on('kicked', (reason) => {
+    console.log('⚠️ Kicked:', reason)
+  })
+
+  bot.on('error', (err) => {
+    console.log('⚠️ Error:', err.message)
+  })
+}
+
+function reconnect() {
+  setTimeout(() => {
+    createBot()
+  }, 5000) // wait 5 seconds before reconnect
+}
+
+createBot()
